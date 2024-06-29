f68a341485e2baece5cc0516c1d852bdcab9adae
Tuan <139671334+tuanluong108@users.noreply.github.com>
2024-06-29 19:16:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
 main() {
-    echo -e "Downloading Latest Roblox"
-    local version="version-dd12d773fa2a4864"
+    echo -e "Downloading Latest Roblox..."
+    if [ "$version" == "version-dd12d773fa2a4864" ]
+    then
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    local version=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer" | ./jq -r ".clientVersionUpload")
     curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    rm ./jq
 
-    echo -e "Installing Latest Roblox"
+    echo -n "Installing Latest Roblox... "
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
     unzip -o -q "./RobloxPlayer.zip"
     mv ./RobloxPlayer.app /Applications/Roblox.app
     rm ./RobloxPlayer.zip
+    echo -e "Done."
 
     echo -e "Downloading MacSploit"
-    curl -O "https://raw.githubusercontent.com/tuanluong108/asdas/main/Macsploit.zip"
+    curl -O "https://raw.githubusercontent.com/Nexus42Dev/MacSploit/main/MacSploit.zip"
 
     echo -e "Installing MacSploit"
-    unzip -o -q "./Macsploit.zip"  # Corrected filename here
+    unzip -o -q "./MacSploit.zip"
+    echo -e "Done."
 
     echo -e "Patching Roblox"
     mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
-    mv ./libdiscord-rpc.dylib "/Applications/Roblox.app/Contents/MacOS/libdiscord-rpc.dylib"
     ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
     mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
-    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
     rm ./insert_dylib
 
     echo -e "Installing MacSploit App"
     [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
     mv ./MacSploit.app /Applications/MacSploit.app
-    rm ./macsploit.zip  # Corrected filename here
+    rm ./MacSploit.zip
     echo -e "Done."
 
-    echo -e "Install Complete! Developed by Nexus42!"
+    echo -e "Install Complete! Developed by mlo!"
 }
 
 main
