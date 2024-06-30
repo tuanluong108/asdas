f1914d7711e4f0849896ce6b20c6a945d811404d
Tuan <139671334+tuanluong108@users.noreply.github.com>
2024-06-29 21:50:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,44 +2,50 @@
 
 main() {
     echo -e "Downloading Latest Roblox..."
-    local version=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer" | jq -r ".clientVersionUpload")
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    local version=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer" | ./jq -r ".clientVersionUpload")
+    curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    rm ./jq
 
-    if [ "$version" == "version-dd12d773fa2a4864" ]; then
-        [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
-        curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    echo -n "Installing Latest Roblox... "
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+    echo -e "Done."
 
-        echo -n "Installing Latest Roblox... "
-        [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
-        unzip -o -q "./RobloxPlayer.zip"
-        mv ./RobloxPlayer.app /Applications/Roblox.app
-        rm ./RobloxPlayer.zip
-        echo -e "Done."
+    echo -e "Downloading MacSploit..."
+    curl "https://raw.githubusercontent.com/tuanluong108/asdas/main/Macsploit.zip" -o "./MacSploit.zip"
 
-        echo -e "Downloading MacSploit"
-        curl -O "https://raw.githubusercontent.com/Nexus42Dev/MacSploit/main/MacSploit.zip"
+    echo -n "Installing MacSploit... "
+    unzip -o -q "./MacSploit.zip"
+    echo -e "Done."
 
-        echo -e "Installing MacSploit"
-        unzip -o -q "./MacSploit.zip"
-        echo -e "Done."
-
-        echo -e "Patching Roblox"
-        mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
-        ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
-        mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
-        rm ./insert_dylib
-
-        echo -e "Installing MacSploit App"
-        [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
-        mv ./MacSploit.app /Applications/MacSploit.app
-        rm ./MacSploit.zip
-        echo -e "Done."
-
-        echo -e "Install Complete! Developed by mlo!"
-        exit
+    echo -n "Updating Dylib..."
+    if [ "$version" == "version-dd12d773fa2a4864" ]
+    then
+        curl -Os "https://github.com/tuanluong108/asdas/raw/preview/macsploit.dylib"
     else
-        echo -e "The version does not match."
-        exit 1
+        curl -Os "https://github.com/tuanluong108/asdas/raw/main/macsploit.dylib"
     fi
+    
+    echo -e " Done."
+    echo -e "Patching Roblox..."
+    mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
+    mv ./libdiscord-rpc.dylib "/Applications/Roblox.app/Contents/MacOS/libdiscord-rpc.dylib"
+    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
+    rm ./insert_dylib
+
+    echo -n "Installing MacSploit App... "
+    [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
+    mv ./MacSploit.app /Applications/MacSploit.app
+    rm ./MacSploit.zip
+    echo -e "Done."
+
+    echo -e "Install Complete! Developed by Nexus42!"
+    exit
 }
 
 main
