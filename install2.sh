df65a074598659d87dcbde55478144d0354a963d
Tuan <139671334+tuanluong108@users.noreply.github.com>
2024-06-30 04:28:02 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,52 +1,36 @@
 #!/bin/bash
 
 main() {
-    clear
-    echo -e "Downloading Latest Roblox..."
+    echo -e "Downloading Latest Roblox"
     [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
-    local version=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer" | ./jq -r ".clientVersionUpload")
-    curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
-    rm ./jq
+    curl "https://setup.rbxcdn.com/mac/version-ecb0dc61c2ff4160-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
 
-    echo -n "Installing Latest Roblox... "
+    echo -e "Installing Latest Roblox"
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
     unzip -o -q "./RobloxPlayer.zip"
     mv ./RobloxPlayer.app /Applications/Roblox.app
     rm ./RobloxPlayer.zip
-    echo -e "Done."
 
-    echo -e "Downloading MacSploit..."
-    curl "https://git.abyssdigital.xyz/main/macsploit.zip" -o "./MacSploit.zip"
+    echo -e "Downloading libHydrogen"
+    rm ./libHydrogen.dylib
+    curl -LJO "https://github.com/tuanluong108/asdas/raw/preview/macsploit.dylib"
 
-    echo -n "Installing MacSploit... "
-    unzip -o -q "./MacSploit.zip"
-    echo -e "Done."
-
-    echo -n "Updating Dylib..."
-    if [ "$version" == "version-dd12d773fa2a4864" ]
-    then
-        curl -Os "https://raw.githubusercontent.com/tuanluong108/asdas/raw/preview/macsploit.dylib"
-    else
-        curl -Os "https://raw.githubusercontent.com/tuanluong108/asdas/raw/main/macsploit.dylib"
-    fi
-    
-    echo -e " Done."
-    echo -e "Patching Roblox..."
-    mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
-    mv ./libdiscord-rpc.dylib "/Applications/Roblox.app/Contents/MacOS/libdiscord-rpc.dylib"
-    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
-    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
-    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
     rm ./insert_dylib
+    echo -e "Downloading insert_dylib"
+    curl -LJO "https://github.com/tuanluong108/asdas/raw/preview/insert_dylib"
 
-    echo -n "Installing MacSploit App... "
-    [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
-    mv ./MacSploit.app /Applications/MacSploit.app
-    rm ./MacSploit.zip
-    echo -e "Done."
+    chmod +x "./insert_dylib"
 
-    echo -e "Install Complete! Developed by Nexus42!"
-    exit
+    echo -e "Patching Roblox"
+    mv ./libHydrogen.dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib"
+    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+
+    chmod +x "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib"
+
+    rm -rf /Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app
+
+    echo -e "Install Complete!"
 }
 
 main
